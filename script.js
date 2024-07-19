f7b796b9b0041c073003e0d4b9eccdae6435c349
Kayvon Kazemini <kayvonk@gmail.com>
2024-07-18 20:35:21 -0600

    added quandrants

--- script.js
+++ script.js
@@ -1983,8 +1983,8 @@ function startBossMovement(enemyClass, enemyImgPath, enemyIndex) {
       ? 750
       : 1000 / ((round + 1) * 0.25);
 
-  // let isAttacking = Math.random() < 0.2;
-  let isAttacking = false;
+  let isAttacking = Math.random() < 0.2;
+  // let isAttacking = false;
   let enemyTimer = setTimeout(() => {
     let enemyImg = document.querySelector(enemyClass);
     if (!enemyImg) {
@@ -2250,6 +2250,18 @@ function bossEnemyMoveRight(enemyClass, enemyImgPath, enemyIndex) {
   }
 }
 
+// ----top half----
+let quadrant1 = ["r1-c1", "r1-c2", "r1-c3", "r2-c1", "r2-c2", "r2-c3", "r3-c1", "r3-c2", "r3-c3" ]
+let quadrant2 = ["r1-c4", "r1-c5", "r1-c6", "r2-c4", "r2-c5", "r2-c6", "r3-c4", "r3-c5", "r3-c6" ]
+let quadrant3 = ["r1-c7", "r1-c8", "r1-c9", "r2-c7", "r2-c8", "r2-c9", "r3-c7", "r3-c8", "r3-c9" ]
+let quadrant4 = ["r1-c10", "r1-c11", "r1-c12", "r2-c10", "r2-c11", "r2-c12", "r3-c10", "r3-c11", "r3-c12" ]
+
+
+// ----bottom half----
+let quadrant5 = ["r4-c1", "r4-c2", "r4-c3", "r5-c1", "r5-c2", "r5-c3", "r6-c1", "r6-c2", "r6-c3" ]
+let quadrant6 = ["r4-c4", "r4-c5", "r4-c6", "r5-c4", "r5-c5", "r5-c6", "r6-c4", "r6-c5", "r6-c6" ]
+let quadrant7 = ["r4-c7", "r4-c8", "r4-c9", "r5-c7", "r5-c8", "r5-c9", "r6-c7", "r6-c8", "r6-c9" ]
+let quadrant8 = ["r4-c10", "r4-c11", "r4-c12", "r5-c10", "r5-c11", "r5-c12", "r6-c10", "r6-c11", "r6-c12" ]
 // --------------end boss-----------
 
 // TODO add boss stage after round 10
